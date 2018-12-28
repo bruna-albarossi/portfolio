cc4000fefce7e4b5b90585f6e4b2231d0f19fe5d
Bruna Albarossi <brunalbarossi@gmail.com>
2018-12-28 11:34:38 -0200

    Portofolio

---
+++ js/main.js
@@ -0,0 +1,54 @@
+var zoomImage = function(id){
+    switch (id) {
+        case 1:
+            $('#modalExemplo').modal('show');
+            var img  =  $('#image-feature');
+            img.attr("src", 'Img/1.jpg');
+            break;
+        case 2: 
+            $('#modalExemplo').modal('show');
+            var img  =  $('#image-feature');
+            img.attr("src", 'Img/2.png');
+            break;
+        case 3: 
+            $('#modalExemplo').modal('show');
+            var img  =  $('#image-feature');
+            img.attr("src", 'Img/3.jpg');
+            break;
+        case 4: 
+            $('#modalExemplo').modal('show');
+            var img  =  $('#image-feature');
+            img.attr("src", 'Img/4.jpg');
+            break;
+        case 5: 
+            $('#modalExemplo').modal('show');
+            var img  =  $('#image-feature');
+            img.attr("src", 'Img/5.jpg');
+            break;
+        case 6: 
+            $('#modalExemplo').modal('show');
+            var img  =  $('#image-feature');
+            img.attr("src", 'Img/6.jpg');
+            break;
+        case 7: 
+            $('#modalExemplo').modal('show');
+            var img  =  $('#image-feature');
+            img.attr("src", 'Img/7.jpg');
+            break;
+        case 8: 
+            $('#modalExemplo').modal('show');
+            var img  =  $('#image-feature');
+            img.attr("src", 'Img/8.jpg');
+            break;
+        case 9: 
+            $('#modalExemplo').modal('show');
+            var img  =  $('#image-feature');
+            img.attr("src", 'Img/9.jpg');
+            break;
+
+        default:
+            return 'img/logo bruna contrato.jpg';
+    }
+
+}
+
